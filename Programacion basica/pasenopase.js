5714b9ce6b939cb741dd2f984449d42a6f4f85f2
AnaisRojas <82849578+AnaisRojas@users.noreply.github.com>
2021-04-26 19:30:38 -0300

    Delete pasenopase.js

--- Programacion basica/pasenopase.js
+++
@@ -1,7 +0,0 @@
-let readlineSync = require("readline-sync");
-let persona = readlineSync.questionFloat("Altura de la persona: ");
-if (persona >= 1.60) {
-    console.log("Pase")
-}else {
-    console.log("Lo siento, no puede pasar");
-};
