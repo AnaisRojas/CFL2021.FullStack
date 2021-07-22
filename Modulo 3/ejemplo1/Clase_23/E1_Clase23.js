07f41c13811eb39ce48b7d342c288a260b3f956e
AnaisRojas <rojasguzmananais@gmail.com>
2021-07-22 11:55:39 -0300

    ejercicios mod 1 y 2

---
+++ Modulo 3/ejemplo1/Clase_23/E1_Clase23.js
@@ -0,0 +1,16 @@
+let readlineSync = require("readline-sync");
+let texto = readlineSync.question("Ingrese un texto: ");
+const chalk = require("chalk");
+
+
+// console.log( texto.toUpperCase());
+let texto2 = chalk.blue(texto)
+
+
+    
+console.log(texto2);
+  
+
+
+
+
